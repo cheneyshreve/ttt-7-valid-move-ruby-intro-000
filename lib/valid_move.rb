00ec9f4cdb1252064a481595f9b27bbe3822c4dc
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-03 18:09:24 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,9 @@ if position_taken?(board,index) == false &&  val.between?(0,8) == true
 else
   return false
 end
+return board
 end
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,index)
  if board[index] == " " || board[index] == "" || board[index] == nil
